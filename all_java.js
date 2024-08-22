29063333c138bf19cf1a70216e39771bfb4c2a75
Caden Allen <cadenallen97@gmail.com>
2024-08-22 18:28:23 -0400

    logo added | inital js improved

--- all_java.js
+++ all_java.js
@@ -11,3 +11,33 @@ function printClick(){
 function hovered(){
     console.log("hovered!");
 }
+
+
+// MEDIA QUERIES | RESPONSIVE DESIGN
+
+
+// :) This section of commenting ends at the next ":)"
+// This section checks for media size, IF the media matches
+// 780px then the media is a mobile device and the website needs to hide
+// certain items and change layout
+
+//holds max-width for media 
+var viewportSize = window.matchMedia("(max-width: 780px)"); 
+
+function checkForMobileDevice(viewportSize) {
+    
+    //If media query matches, hide navigation bar items...,
+    //only show tab menu on navigation bar and logo
+    if(viewportSize.matches) {
+        
+    }
+}
+
+checkForMobileDevice(viewportSize);
+
+viewportSize.addEventListener("change", function() {
+    checkForMobileDevice(viewportSize);
+})
+
+// :) End of commenting for above section
+
